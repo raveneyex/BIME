86a0edf038b60b68ab4eb7915db6747011a91592
Andres Ossa <andres.ossa@cision.com>
2017-12-24 14:40:02 -0500

    Semantically correct HTML (sort off)

--- src/components/Periods/PeriodInfo.js
+++ src/components/Periods/PeriodInfo.js
@@ -3,13 +3,13 @@ import React from 'react';
 const PeriodInfo = ({ period, onClick }) => {
     const { name, startDate, endDate } = period; 
     return (
-        <div className="period-info" onClick={onClick}>
+        <li className="period-info" onClick={onClick}>
             <h2 className="period-title">{name}</h2>
             <div className="period-dates">
                 <span className="startDate">{startDate}</span>
                 <span className="endDate">{endDate}</span>
             </div>
-        </div>
+        </li>
     );
 };
 
